3942650b961d08b20ddf65d67c5305f26e553c9c
Christoph Dorn <christoph@christophdorn.com>
2015-11-16 11:11:53 -0800

    wip

--- 0-window.api.js
+++ 0-window.api.js
@@ -14,3 +14,39 @@ exports.forLib = function (LIB) {
 
     return exports;
 }
+
+
+/*		
+		window.Cores.load.adapters.pinf.load(
+			"/dist/DependencyVisualization.bundle.js"
+		).then(function (container) {
+
+			container.main();
+
+		}).catch(function (err) {
+			console.log("ERROR loading components using pinf loader:", err.stack);
+		});
+
+		window.Cores.load.adapters.requirejs.load(
+			"/dist/DependencyVisualization.amd.js"
+		).then(function (container) {
+
+			container.main();
+
+		}).catch(function (err) {
+			console.log("ERROR loading components using requirejs loader:", err.stack);
+		});
+*/
+
+/*
+		window.Cores.load.adapters.systemjs.load(
+			"/dist/DependencyVisualization.amd.js"
+		).then(function (container) {
+
+			container.main();
+
+		}).catch(function (err) {
+			console.log("ERROR loading components using systemjs loader:", err.stack);
+		});
+*/
+
